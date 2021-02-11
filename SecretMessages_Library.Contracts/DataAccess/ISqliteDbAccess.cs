8296a337d56835c36a2ea95d80a76806255686cb
Mariliis Jaago <mariliisjaago@hotmail.com>
2021-02-11 22:12:10 +0200

    Add project files.

---
+++ SecretMessages_Library.Contracts/DataAccess/ISqliteDbAccess.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace SecretMessages_Library.Contracts.DataAccess
+{
+    public interface ISqliteDbAccess
+    {
+        List<T> LoadData<T, U>(string sqlStatement, U parameters, string connectionStringName);
+        void SaveData<U>(string sqlStatement, U parameters, string connectionStringName);
+    }
+}
---
+++ SecretMessages_Library/DataAccess/SqliteDbAccess.cs
@@ -0,0 +1,41 @@
+﻿using Dapper;
+using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SQLite;
+using System.Linq;
+
+namespace SecretMessages_Library.DataAccess
+{
+    public class SqliteDbAccess : ISqliteDbAccess
+    {
+        private readonly IConfiguration _config;
+
+        public SqliteDbAccess(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public List<T> LoadData<T, U>(string sqlStatement, U parameters, string connectionStringName)
+        {
+            string connectionString = _config.GetConnectionString(connectionStringName);
+
+            using (IDbConnection connection = new SQLiteConnection(connectionString))
+            {
+                List<T> data = connection.Query<T>(sqlStatement, parameters).ToList();
+
+                return data;
+            }
+        }
+
+        public void SaveData<U>(string sqlStatement, U parameters, string connectionStringName)
+        {
+            string connectionString = _config.GetConnectionString(connectionStringName);
+
+            using (IDbConnection connection = new SQLiteConnection(connectionString))
+            {
+                connection.Execute(sqlStatement, parameters);
+            }
+        }
+    }
+}
