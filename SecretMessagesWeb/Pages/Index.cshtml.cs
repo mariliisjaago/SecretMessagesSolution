d1e49103ec3b0a5cfa4d86896b0f8a81804e02b8
Mariliis Jaago <mariliisjaago@hotmail.com>
2021-02-23 22:01:11 +0200

    Made the development branch, installed NuGet and configured sessioning in Startup.cs

--- SecretMessagesWeb/Pages/Index.cshtml.cs
+++ SecretMessagesWeb/Pages/Index.cshtml.cs
@@ -1,23 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace SecretMessagesWeb.Pages
 {
+
+    [BindProperties]
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
 
-        [BindProperty]
         public string UserName { get; set; }
-        [BindProperty]
+
         public string Password { get; set; }
 
-        [BindProperty]
         public bool FailedLoginResult { get; set; } = false;
 
         public IndexModel(ILogger<IndexModel> logger)
